c447e71ff02ae58e0160a376ee48f7a774a3891f
Mads DJ <madsdj@gmail.com>
2019-06-23 23:37:36 +0200

    Added MVVM helper class library

---
+++ Bismuth.Mvvm/RelayCommand.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Input;
+
+namespace Bismuth.Mvvm
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = null;
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+        }
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+    }
+
+    public class RelayCommand<TArg> : ICommand
+    {
+        private readonly Action<TArg> _execute;
+        private readonly Func<TArg, bool> _canExecute;
+
+        public RelayCommand(Action<TArg> execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = null;
+        }
+
+        public RelayCommand(Action<TArg> execute, Func<TArg, bool> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+        }
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            TArg arg = (TArg)parameter;
+            return _canExecute == null || _canExecute(arg);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            TArg arg = (TArg)parameter;
+            _execute(arg);
+        }
+    }
+}
