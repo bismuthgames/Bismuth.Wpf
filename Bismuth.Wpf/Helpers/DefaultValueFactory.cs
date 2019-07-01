fd3db1dde4117d9b1c2ea88f4278e65a043ec976
Mads DJ <madsdj@gmail.com>
2019-07-01 23:14:20 +0200

    Created DefaultValueFactory helper class
    This is a helper class to workaround a missing WPF feature -- creating non-shared default values of dependency properties.

---
+++ Bismuth.Wpf/Helpers/DefaultValueFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+
+namespace Bismuth.Wpf.Helpers
+{
+    public static class DefaultValueFactory
+    {
+        public static object Create(Freezable defaultValue)
+        {
+            return typeof(DependencyProperty)
+                .Assembly
+                .GetType("MS.Internal.FreezableDefaultValueFactory")
+                .GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, new[] { typeof(Freezable) }, null)
+                .Invoke(new[] { defaultValue });
+        }
+
+        public static object CreateObservableCollection<T>()
+        {
+            return typeof(DependencyProperty)
+                .Assembly
+                .GetType("MS.Internal.ObservableCollectionDefaultValueFactory`1")
+                .MakeGenericType(typeof(T))
+                .GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, null, new Type[0], null)
+                .Invoke(null);
+        }
+    }
+}
