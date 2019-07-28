0bf28eb7a825c1c5033eb5752a02e84294b12ba4
Mads DJ <madsdj@gmail.com>
2019-07-29 00:20:07 +0200

    Added some tests where the SelectedItems source is set to an array

--- Bismuth.Wpf.Demo/ViewModels/MultiSelectTreeViewDemoViewModel.cs
+++ Bismuth.Wpf.Demo/ViewModels/MultiSelectTreeViewDemoViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Bismuth.Mvvm;
 
 namespace Bismuth.Wpf.Demo.ViewModels
@@ -33,7 +34,13 @@ namespace Bismuth.Wpf.Demo.ViewModels
         }
 
         public IList<NodeViewModel> RootNodes { get; }
-        public IList<NodeViewModel> SelectedNodes { get; } = new ObservableCollection<NodeViewModel>();
+
+        private IList<NodeViewModel> _selectedNodes = new ObservableCollection<NodeViewModel>();
+        public IList<NodeViewModel> SelectedNodes
+        {
+            get { return _selectedNodes; }
+            set { Set(ref _selectedNodes, value); }
+        }
 
         private int _idCounter = 0;
         private string GetName()
@@ -45,6 +52,37 @@ namespace Bismuth.Wpf.Demo.ViewModels
         {
             foreach (var rootNode in RootNodes)
                 rootNode.RemoveCallback += () => RootNodes.Remove(rootNode);
+        }
+
+        public ICommand FixedSizeTest1Command => new RelayCommand(FixedSizeTest1);
+
+        public void FixedSizeTest1()
+        {
+            SelectedNodes = new[]
+            {
+                RootNodes[0].Children[1],
+                RootNodes[0].Children[1].Children[0],
+                RootNodes[0].Children[1].Children[1],
+            };
+        }
+
+        public ICommand FixedSizeTest2Command => new RelayCommand(FixedSizeTest2);
+
+        public void FixedSizeTest2()
+        {
+            SelectedNodes = new[]
+            {
+                RootNodes[1].Children[0],
+                RootNodes[1].Children[0].Children[1],
+                RootNodes[1].Children[0].Children[2],
+            };
+        }
+
+        public ICommand ResetCommand => new RelayCommand(Reset);
+
+        public void Reset()
+        {
+            SelectedNodes = new ObservableCollection<NodeViewModel>();
         }
     }
 }
