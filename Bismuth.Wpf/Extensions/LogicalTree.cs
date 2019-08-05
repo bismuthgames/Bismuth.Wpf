5662bfb552340ec270666e46025f4b32f5a777a4
Mads DJ <madsdj@gmail.com>
2019-08-05 23:17:21 +0200

    Added visual and logical tree extensions

---
+++ Bismuth.Wpf/Extensions/LogicalTree.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Bismuth.Wpf.Extensions
+{
+    public static class LogicalTree
+    {
+        public static T FindLogicalParent<T>(this DependencyObject current) where T : DependencyObject
+        {
+            return current
+                .EnumerateLogicalParents()
+                .OfType<T>()
+                .FirstOrDefault();
+        }
+
+        public static T FindLogicalParentByName<T>(this DependencyObject current, string name) where T : DependencyObject
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            return current
+                .EnumerateLogicalParents()
+                .OfType<T>()
+                .FirstOrDefault(parent => parent.MatchOnName(name));
+        }
+
+        public static T FindLogicalChildren<T>(this DependencyObject current) where T : DependencyObject
+        {
+            return current
+                .EnumerateLogicalChildren()
+                .OfType<T>()
+                .FirstOrDefault();
+        }
+
+        public static T FindLogicalChildByName<T>(this DependencyObject current, string name) where T : DependencyObject
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            return current
+                .EnumerateLogicalParents()
+                .OfType<T>()
+                .FirstOrDefault(child => child.MatchOnName(name));
+        }
+
+        public static IEnumerable<DependencyObject> EnumerateLogicalParents(this DependencyObject current)
+        {
+            var parent = LogicalTreeHelper.GetParent(current);
+            while (parent != null)
+            {
+                yield return parent;
+                parent = LogicalTreeHelper.GetParent(parent);
+            }
+        }
+
+        public static IEnumerable<DependencyObject> EnumerateLogicalChildren(this DependencyObject current)
+        {
+            foreach (DependencyObject child in LogicalTreeHelper.GetChildren(current))
+            {
+                yield return child;
+                foreach (var childOfChild in child.EnumerateLogicalChildren())
+                    yield return childOfChild;
+            }
+        }
+
+        private static bool MatchOnName(this DependencyObject obj, string name)
+        {
+            return obj is FrameworkElement element && name.Equals(element.Name, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ Bismuth.Wpf/Extensions/VisualTree.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Bismuth.Wpf.Extensions
+{
+    public static class VisualTree
+    {
+        public static T FindVisualParent<T>(this DependencyObject current) where T : DependencyObject
+        {
+            return current
+                .EnumerateVisualParents()
+                .OfType<T>()
+                .FirstOrDefault();
+        }
+
+        public static T FindVisualParentByName<T>(this DependencyObject current, string name) where T : DependencyObject
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            return current
+                .EnumerateVisualParents()
+                .OfType<T>()
+                .FirstOrDefault(parent => parent.MatchOnName(name));
+        }
+
+        public static T FindVisualChildren<T>(this DependencyObject current) where T : DependencyObject
+        {
+            return current
+                .EnumerateVisualChildren()
+                .OfType<T>()
+                .FirstOrDefault();
+        }
+
+        public static T FindVisualChildByName<T>(this DependencyObject current, string name) where T : DependencyObject
+        {
+            if (name == null) throw new ArgumentNullException(nameof(name));
+
+            return current
+                .EnumerateVisualParents()
+                .OfType<T>()
+                .FirstOrDefault(child => child.MatchOnName(name));
+        }
+
+        public static IEnumerable<DependencyObject> EnumerateVisualParents(this DependencyObject current)
+        {
+            var parent = VisualTreeHelper.GetParent(current);
+            while (parent != null)
+            {
+                yield return parent;
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+        }
+
+        public static IEnumerable<DependencyObject> EnumerateVisualChildren(this DependencyObject current)
+        {
+            int childrenCount = VisualTreeHelper.GetChildrenCount(current);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(current, i);
+                yield return child;
+                foreach (var childOfChild in child.EnumerateVisualChildren())
+                    yield return childOfChild;
+            }
+        }
+
+        private static bool MatchOnName(this DependencyObject obj, string name)
+        {
+            return obj is FrameworkElement element && name.Equals(element.Name, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
