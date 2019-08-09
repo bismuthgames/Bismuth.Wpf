d88894d4fad18351e836e31c6f91e41af9cad3ae
Mads DJ <madsdj@gmail.com>
2019-08-09 21:24:34 +0200

    Added converters

---
+++ Bismuth.Wpf/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Bismuth.Wpf.Converters
+{
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public Visibility TrueValue { get; set; } = Visibility.Visible;
+        public Visibility FalseValue { get; set; } = Visibility.Collapsed;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? TrueValue : FalseValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (Visibility)value == TrueValue;
+        }
+    }
+}
---
+++ Bismuth.Wpf/Converters/DepthToIndentConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Bismuth.Wpf.Extensions;
+
+namespace Bismuth.Wpf.Converters
+{
+    public class DepthToIndentConverter : IValueConverter
+    {
+        public double IndentLength { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TreeViewItem item)
+                return new Thickness(GetDepth(item) * IndentLength, 0, 0, 0);
+
+            return new Thickness(0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        private int GetDepth(TreeViewItem item)
+        {
+            var parent = item.FindVisualParent<TreeViewItem>();
+            if (parent != null) return GetDepth(parent) + 1;
+
+            return 0;
+        }
+    }
+}
