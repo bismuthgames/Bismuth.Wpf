656072ed0b409ca0d0ea25cc6bd1afe981572bb0
Mads DJ <madsdj@gmail.com>
2019-08-09 21:32:05 +0200

    Added DragDropTreeView

---
+++ Bismuth.Wpf/Controls/DragDropTreeView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bismuth.Wpf.Controls
+{
+    public class DragDropTreeView : MultiSelectTreeView
+    {
+        static DragDropTreeView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DragDropTreeView), new FrameworkPropertyMetadata(typeof(DragDropTreeView)));
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new DragDropTreeViewItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is DragDropTreeViewItem;
+        }
+    }
+}
---
+++ Bismuth.Wpf/Controls/DragDropTreeViewItem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bismuth.Wpf.Controls
+{
+    public class DragDropTreeViewItem : MultiSelectTreeViewItem
+    {
+        static DragDropTreeViewItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DragDropTreeViewItem), new FrameworkPropertyMetadata(typeof(DragDropTreeViewItem)));
+        }
+
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new DragDropTreeViewItem();
+        }
+
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is DragDropTreeViewItem;
+        }
+    }
+}
