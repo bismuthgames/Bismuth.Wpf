5d53df244154722dfd9556721593a1f532d22df1
Mads DJ <madsdj@gmail.com>
2019-08-11 01:05:17 +0200

    Styled DragDropTreeViewItem

--- Bismuth.Wpf.Demo/ViewModels/DragDropNodeViewModel.cs
+++ Bismuth.Wpf.Demo/ViewModels/DragDropNodeViewModel.cs
@@ -1,14 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
+using Bismuth.Mvvm;
 
 namespace Bismuth.Wpf.Demo.ViewModels
 {
     public class DragDropNodeViewModel : NodeViewModel
     {
         public DragDropNodeViewModel(string name, params NodeViewModel[] children) : base(name, children)
+        {
+        }
+
+        public Func<object, bool> AcceptFunction { get; } = item => true;
+        public Type[] AcceptTypes { get; } = new[] { typeof(object[]) };
+
+        public ICommand DropCommand => new RelayCommand<DragDropNodeViewModel[]>(Drop);
+
+        public void Drop(DragDropNodeViewModel[] nodes)
+        {
+        }
+
+        public ICommand DropBeforeCommand => new RelayCommand<DragDropNodeViewModel[]>(DropBefore);
+
+        public void DropBefore(DragDropNodeViewModel[] nodes)
+        {
+        }
+
+        public ICommand DropAfterCommand => new RelayCommand<DragDropNodeViewModel[]>(DropAfter);
+
+        public void DropAfter(DragDropNodeViewModel[] nodes)
         {
         }
     }
