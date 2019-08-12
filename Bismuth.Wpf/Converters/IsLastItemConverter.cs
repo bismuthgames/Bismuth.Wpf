ca4e73e1221fd791b78a423c7cc19a3f054a023a
Mads DJ <madsdj@gmail.com>
2019-08-12 23:22:03 +0200

    Created custom style for TabControl

---
+++ Bismuth.Wpf/Converters/IsLastItemConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Bismuth.Wpf.Converters
+{
+    public class IsLastItemConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var item = (DependencyObject)value;
+            var itemContainerGenerator = ItemsControl.ItemsControlFromItemContainer(item).ItemContainerGenerator;
+
+            int index = itemContainerGenerator.IndexFromContainer(item);
+            return index == itemContainerGenerator.Items.Count - 1;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ Bismuth.Wpf/Converters/IsPreviousItemSelectedConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Bismuth.Wpf.Converters
+{
+    public class IsPreviousItemSelectedConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            var item = (DependencyObject)values[0];
+            int index = ItemsControl.ItemsControlFromItemContainer(item).ItemContainerGenerator.IndexFromContainer(item);
+            int selectedIndex = (int)values[1];
+            return selectedIndex == index - 1;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
