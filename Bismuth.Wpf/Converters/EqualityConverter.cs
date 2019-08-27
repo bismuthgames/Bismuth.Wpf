4330ed4e05389999a8d0379ab9b7aa0810f37802
Mads DJ <madsdj@gmail.com>
2019-08-27 23:30:23 +0200

    Implemented default value indicator

---
+++ Bismuth.Wpf/Converters/EqualityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Bismuth.Wpf.Converters
+{
+    public class EqualityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 2) throw new ArgumentException("Values must be of length two.", nameof(values));
+
+            return object.Equals(values[0], values[1]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
