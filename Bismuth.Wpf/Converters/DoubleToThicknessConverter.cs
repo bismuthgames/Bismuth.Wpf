a71b11311feed064103602c2a73c1e317a0ed37d
Mads DJ <madsdj@gmail.com>
2020-02-06 00:28:44 +0100

    Updated DragDropTreeViewStyle

---
+++ Bismuth.Wpf/Converters/DoubleToThicknessConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Bismuth.Wpf.Converters
+{
+    public class DoubleToThicknessConverter : IValueConverter
+    {
+        public Thickness Scale { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double scale = System.Convert.ToDouble(value);
+            return new Thickness
+            (
+                scale * Scale.Left,
+                scale * Scale.Top,
+                scale * Scale.Right,
+                scale * Scale.Bottom
+            );
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
