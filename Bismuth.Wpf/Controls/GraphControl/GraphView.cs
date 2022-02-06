cabefbd4400a43b34ce070614aef766cde81536b
Mads DJ <madsdj@gmail.com>
2022-02-07 00:23:57 +0100

    CompositeCollection

--- Bismuth.Wpf/Controls/GraphControl/GraphView.cs
+++ Bismuth.Wpf/Controls/GraphControl/GraphView.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Linq;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace Bismuth.Wpf.Controls
 {
@@ -10,6 +14,21 @@ namespace Bismuth.Wpf.Controls
         static GraphView()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(GraphView), new FrameworkPropertyMetadata(typeof(GraphView)));
+        }
+
+        private readonly CompositeCollection _cc = new CompositeCollection();
+        private readonly CollectionContainer _cc1 = new CollectionContainer();
+        private readonly CollectionContainer _cc2 = new CollectionContainer();
+
+        public GraphView()
+        {
+            _cc.Add(_cc1);
+            _cc.Add(_cc2);
+
+            _cc1.Collection = Items;
+            _cc2.Collection = Connections;
+
+            _connectiosHost.ItemsSourceChanged += (s, e) => _cc2.Collection = e ?? _connectiosHost.Items;
         }
 
         public IEnumerable ConnectionsSource
@@ -32,6 +51,11 @@ namespace Bismuth.Wpf.Controls
             DependencyProperty.Register(nameof(ConnectionTemplate), typeof(DataTemplate), typeof(GraphView), new PropertyMetadata(
                 (d, e) => ((GraphView)d)._connectiosHost.ItemTemplate = (DataTemplate)e.NewValue));
 
+        protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
+        {
+            _cc1.Collection = newValue ?? Items;
+        }
+
         protected override DependencyObject GetContainerForItemOverride()
         {
             return new GraphNode();
@@ -48,6 +72,9 @@ namespace Bismuth.Wpf.Controls
         {
             var panel = (Panel)GetTemplateChild("XYZ");
             panel.Children.Add(_connectiosHost);
+
+            var merge = (ItemsControl)GetTemplateChild("Merge");
+            merge.ItemsSource = _cc;
         }
 
         private void ApplyHost()
@@ -62,6 +89,18 @@ namespace Bismuth.Wpf.Controls
 
         private class GraphConnectionHost : ItemsControl
         {
+            public GraphConnectionHost()
+            {
+                Template = null;
+            }
+
+            protected override void OnItemsSourceChanged(IEnumerable oldValue, IEnumerable newValue)
+            {
+                ItemsSourceChanged(this, newValue);
+            }
+
+            public event EventHandler<IEnumerable> ItemsSourceChanged;
+
             protected override DependencyObject GetContainerForItemOverride()
             {
                 return new GraphConnection();
